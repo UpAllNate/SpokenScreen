06138c8624827f131b33db31585d3128665b315a
UpAllNate <UpAllNate@gmail.com>
2023-09-07 02:15:11 -0400

    considering callouts for parsing

--- core.py
+++ core.py
@@ -11,13 +11,12 @@ class SpokenScreenApplication:
         self.setup_complete = False
         self.los_dict = {}
 
-    def execute_LOS(self) -> FunReturn:
+    def extract_program(self) -> tuple[bool,  str]:
+
+        success, prog = False, ""
 
         if not self.file_los.detect() or self.file_los.type is not PathType.FILE:
-            return FunReturn(success= False)
-        
-        scanning_setup = False
-        parsing_var_Color = False
+            return success, prog
 
         with open(self.file_los.path_str, 'r') as los:
 
@@ -37,39 +36,50 @@ class SpokenScreenApplication:
                 if not line:
                     continue
 
-                # Detect when scanning lines within setup (executes once)
-                if line == "@setup_start":
-                    scanning_setup = True
-                    continue
-                
-                if line == "@setup_end" and scanning_setup:
-                    scanning_setup = False
-                    self.setup_complete = True
-                    continue
-                
-                # Ignore setup lines if setup has already been parsed
-                if scanning_setup and self.setup_complete:
-                    continue
+                # line += " @line_end "
 
-                # Detect if there is an assignment in this line
-                if "=" in line:
+                if prog == "":
+                    prog = line
+                else:
+                    prog += " " + line
+        
+        for c in ["[", "]", "(", ")", ","]:
+            prog = prog.replace(c, f" {c} ")
 
-                    # Detect if this is a function call
-                    if "(" in line:
+        while "  " in prog:
+            prog = prog.replace("  ", " ")
 
-                
+        prog = prog.split(" ")
+            
+        success = True
 
-                
+        return success, prog
 
+    def execute_program(prog : str) -> None:
+        prog = prog.split(" ")
 
+        scanning_setup = False
+        parsing_var_Color = False
 
-                    
-                
+        # # Detect when scanning lines within setup (executes once)
+        # if line == "@setup_start":
+        #     scanning_setup = True
+        #     continue
 
-                
+        # if line == "@setup_end" and scanning_setup:
+        #     scanning_setup = False
+        #     self.setup_complete = True
+        #     continue
 
-                
+        # # Ignore setup lines if setup has already been parsed
+        # if scanning_setup and self.setup_complete:
+        #     continue
 
+        # # Detect if there is an assignment in this line
+        # if "=" in line:
+
+        #     # Detect if this is a function call
+        #     if "(" in line:
 
     
 if __name__ == "__main__":
@@ -79,6 +89,7 @@ if __name__ == "__main__":
         path_obj=Path.joinpath(SSPath.profiles.path_obj, "PokeFR", "main.los")
     )
 
-    print(mainLOS)
+    app_blueTB = SpokenScreenApplication(mainLOS)
 
-    SpokenScreenApplication.execute_LOS(mainLOS)
+    success, prog = app_blueTB.extract_program()
+    print(prog)
